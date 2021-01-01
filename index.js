00a8f57f2a701df5575977f87594c60d9969f306
Artem <artskadin@gmail.com>
2021-01-02 02:14:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import bodyParser from 'body-parser';
 import {createReadStream} from 'fs';
 import crypto from 'crypto';
 import http from 'http';
-import Zombie from 'zombie';
 import mongodb from 'mongodb';
 
 import appSrc from './app.js';
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongodb, Zombie);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongodb);
 app.listen(process.env.PORT || 3400);
